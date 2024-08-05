3dfc69da8ffa7cea0b59cdfa10eae8850e9b8e53
mkapil465 <keshavgodani10@gmail.com>
2024-08-05 14:57:04 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'deilify-ui-vendor',
+  webDir: 'www'
+};
+
+export default config;
